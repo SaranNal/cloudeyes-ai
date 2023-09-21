59d7663fbb1268bc8c0ab069fd876085ee831984
Sanju Rao <sanjooprao@knackforge.com>
2023-09-21 15:25:10 +0530

    Confuring script to run in batch job

--- scripts/process-utilization.py
+++ scripts/process-utilization.py
@@ -1,11 +1,41 @@
+import sys
 import json
+import boto3
 import datetime
 from dateutil.tz import tzutc
+# from .helper import get_settings
 from collections import defaultdict
-from app.db_utility import get_customer_db
+# from app.db_utility import get_customer_db
+from botocore.exceptions import BotoCoreError
 
-with open('daily-utilization.json') as metric_file:
-    metric_data = json.load(metric_file)
+# get env variables
+# settings = get_settings()
+# print(settings)
+# sys.exit(1)
+
+# Fetching the File name
+if len(sys.argv) > 1:
+    file_key = sys.argv[1]
+    print(f"Processing file: {file_key}")
+else:
+    print("No S3 file name provided")
+    sys.exit(1)
+
+# Initialize the S3 client
+s3 = boto3.client('s3')
+bucket_name = 'cloudeyes'
+
+try:
+    # Fetch the file and read its contents
+    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+    metric_data = json.loads(obj['Body'].read().decode('utf-8'))
+    print(file_content)
+except json.JSONDecodeError as e:
+    print("JSON decoding fail")
+    sys.exit(1)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    sys.exit(1)
 
 
 def dict_hepler(): return defaultdict(dict_hepler)
