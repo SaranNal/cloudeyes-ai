79df4e400e262a43c4741c4663ec890f9180cbf3
Suresh R <suresh.r@knackforge.com>
2023-09-26 16:11:02 +0530

    fixed main.py

--- app/main.py
+++ app/main.py
@@ -30,13 +30,12 @@ app.add_middleware(
 )
 
 
-settings = get_settings()
 api_key_header = APIKeyHeader(name="access_token", auto_error=False)
 
 
 async def get_api_key(api_key_header: str = Security(api_key_header)):
     """Validate the API key in the header"""
-    if api_key_header == settings.api_key:
+    if api_key_header == get_settings('api_key'):
         return api_key_header
     else:
         raise HTTPException(
