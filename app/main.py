29cd56b2c33dfd1712926c97833b3a56338e6dea
Sanju Rao <sanjooprao@knackforge.com>
2023-10-12 19:44:47 +0530

    commiting auth

--- app/main.py
+++ app/main.py
@@ -50,21 +50,20 @@ async def get_api_key(api_key_header: str=Security(api_key_header)):
             status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
         )
 
+# @app.middleware("http")
+# async def cognito_authenticate(request: Request, call_next):
+#     try:
+#         token = request.headers["Authorization"]
+#     except KeyError:
+#         # return HTTPException(status_code=401)
+#         return JSONResponse(status_code=401, content="Authentication missing")  # or 401
 
-@app.middleware("http")
-async def cognito_authenticate(request: Request, call_next):
-    try:
-        token = request.headers["Authorization"]
-    except KeyError:
-        # return HTTPException(status_code=401)
-        return JSONResponse(status_code=401, content="Authentication missing")  # or 401
-
-    verification_of_token = cognito_validate(token)
-    if verification_of_token:
-        response = await call_next(request)
-        return response
-    else:
-        return JSONResponse(status_code=401, content="Authentication failed")  # or 401
+#     verification_of_token = cognito_validate(token)
+#     if verification_of_token:
+#         response = await call_next(request)
+#         return response
+#     else:
+#         return JSONResponse(status_code=401, content="Authentication failed")  # or 401
 
 
 @app.get("/")
