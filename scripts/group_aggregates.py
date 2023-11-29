4af030daa43fd655f3140f0bc4c61665d8c24026
Suresh R <suresh.r@knackforge.com>
2023-11-29 18:49:46 +0530

    removed exit

--- scripts/group_aggregates.py
+++ scripts/group_aggregates.py
@@ -1,10 +1,8 @@
 import scripts.aggregate_billing as aggregate_billing
 import scripts.aggregate_utilization as aggregate_utilization
 import scripts.aggregate_security_recommendation as aggregate_security_recommendation
-import os
 
 print("Processing aggregate billing data...")
-os._exit()
 aggregate_billing()
 print("Processing aggregate utilization data...")
 aggregate_utilization()
