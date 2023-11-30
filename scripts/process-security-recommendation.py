8a1875adb24ab1e15ca7a36dae0f5f0375866953
Suresh R <suresh.r@knackforge.com>
2023-11-30 17:13:46 +0530

    fixed security recommendations

--- scripts/process-security-recommendation.py
+++ scripts/process-security-recommendation.py
@@ -14,18 +14,22 @@ def dict_helper(): return defaultdict(dict_helper)
 
 attributes = ["name", "description", "metadata"]
 
-for customer_id, accounts_data in recommendations_unprocessed.items():
+for customer_records in recommendations_unprocessed:
     preprocessed_recommendations = []
     processed_recommendations = dict_helper()
-    for account_data in accounts_data:
-        for category, data_list in account_data['recommendations'].items():
-            processed_recommendations[customer_id][category] = []
+    for accounts_data in customer_records['data']:
+        customer_id = customer_records['user_id']
+        account_id = accounts_data['account_id']
+        processed_recommendations[account_id]['account_id'] = account_id
+        for category, data_list in accounts_data['recommendations'].items():
+            processed_recommendations[account_id][category] = []
             for data in data_list:
                 data_dict = {}
                 for key, val in data.items():
                     if key in attributes:
                         data_dict[key] = val
-                processed_recommendations[category].append(data_dict)
+                processed_recommendations[account_id][category].append(
+                    data_dict)
     preprocessed_recommendations.append(processed_recommendations)
 
     customer_db = get_database(customer_id)
