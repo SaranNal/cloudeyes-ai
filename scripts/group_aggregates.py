55a676f324e15c62ac09ceec6affef14a9ab82b4
Sanju Rao <sanjooprao@knackforge.com>
2023-11-30 12:08:43 +0530

    Fixed group aggregation

--- scripts/group_aggregates.py
+++ scripts/group_aggregates.py
@@ -1,12 +1,10 @@
-import scripts.aggregate_billing as aggregate_billing
-import scripts.aggregate_utilization as aggregate_utilization
-import scripts.aggregate_security_recommendation as aggregate_security_recommendation
-import os
+from scripts.aggregate_billing import aggregate_billing
+from scripts.aggregate_utilization import aggregate_utilization
+from scripts.aggregate_security_recommendation import aggregated_security_recommendation
 
 print("Processing aggregate billing data...")
-os._exit()
 aggregate_billing()
 print("Processing aggregate utilization data...")
 aggregate_utilization()
 print("Processing aggregate security and recommendation data...")
-aggregate_security_recommendation()
+aggregated_security_recommendation()
